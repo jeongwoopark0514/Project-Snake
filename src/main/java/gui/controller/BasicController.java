8b9eab003a7420bae65c4fef24f66083c2499da5
Jeongwoo <j.park-3@student.tudelft.nl>
2019-11-19 00:20:31 +0100

    Add splash screen and relevent resources. Adjust Main class a bit. Edit gitignore. - need further change for PMD

---
+++ src/main/java/gui/controller/BasicController.java
@@ -0,0 +1,15 @@
+package gui.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BasicController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("nothing implemented");
+    }
+}
