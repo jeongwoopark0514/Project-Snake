41f58021933e27b7af5886bc2ccda4556aa0aeaf
Jeongwoo <j.park-3@student.tudelft.nl>
2019-12-03 17:19:26 +0100

    Fix checkstyle

--- src/main/java/gui/controller/PasswordHash.java
+++ src/main/java/gui/controller/PasswordHash.java
@@ -1,6 +1,5 @@
 package gui.controller;
 
-import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -30,10 +29,9 @@ public class PasswordHash {
     public PasswordHash(String password) {
         this.password = password;
     }
+
     /**
      * Returns a salted PBKDF2 hash of the password.
-     *
-     * //@param password the password to hash
      * @return a salted PBKDF2 hash of the password
      */
     public String createHash()
@@ -62,8 +60,6 @@ public class PasswordHash {
 
     /**
      * Validates a password using a hash.
-     *
-     * //@param password the password to check
      * @param goodHash the hash of the valid password
      * @return true if the password is correct, false if not
      */
