4a7c6d5ab7aaf672dc82a21c1ed9373a149c5d8e
Avonite <M.C.denHeijer-1@student.tudelft.nl>
2019-12-05 16:55:14 +0100

    Reintroduce SnakeApp for testing purposes

--- src/main/java/game/SnakeApp.java
+++ src/main/java/game/SnakeApp.java
@@ -1,39 +1,44 @@
-//package game;
-//
-//import static game.Directions.DOWN;
-//import static game.GameSettings.BACKGROUND_COLOR;
-//import static game.GameSettings.HEIGHT;
-//import static game.GameSettings.WIDTH;
-//
-//import javafx.application.Application;
-//import javafx.scene.Group;
-//import javafx.scene.Scene;
-//import javafx.scene.canvas.Canvas;
-//import javafx.scene.canvas.GraphicsContext;
-//import javafx.stage.Stage;
-//
-//public class SnakeApp extends Application {
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//        Group root = new Group();
-//        Scene scene = new Scene(root, WIDTH, HEIGHT, BACKGROUND_COLOR);
-//
-//        final Canvas canvas = new Canvas(WIDTH, HEIGHT);
-//
-//        final GraphicsContext gc = canvas.getGraphicsContext2D();
-//
-//        root.getChildren().add(canvas);
-//
-//        primaryStage.setScene(scene);
-//        primaryStage.setTitle("Snake");
-//        primaryStage.show();
-//
-//        Game game = new Game(scene, gc, canvas, new Snake(new Point(10, 10), DOWN));
-//
-//        game.start();
-//    }
-//}
+package game;
+
+import static game.Directions.DOWN;
+import static game.GameSettings.BACKGROUND_COLOR;
+import static game.GameSettings.HEIGHT;
+import static game.GameSettings.WIDTH;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+
+/**
+ * This class is only used for testing purposes. This way we can directly go
+ * to the game screen and we do not have to click through splash and register screen.
+ */
+public class SnakeApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, WIDTH, HEIGHT, BACKGROUND_COLOR);
+
+        final Canvas canvas = new Canvas(WIDTH, HEIGHT);
+
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        root.getChildren().add(canvas);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Snake");
+        primaryStage.show();
+
+        Painter painter = new Painter(gc);
+        Game game = new Game(scene, painter, canvas, new Snake(new Point(10, 10), DOWN));
+
+        game.start();
+    }
+}
