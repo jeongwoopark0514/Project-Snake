ae7a1cd7db6550d3738b095798ac0072362ce439
Rohan Deshamudre <deshamudreshivadarshanrohan@student.tudelft.nl>
2019-12-21 00:01:23 +0100

    setup table to save scores

--- setup.sql
+++ setup.sql
@@ -1,11 +1,18 @@
 -- SEM Snake database setup script.
 
 -- Delete any old data
-drop table if exists users;
+drop table if exists users,scores;
 
 -- Create the table
 create table users
 (
   username varchar(20) not null primary key,
   password varchar(200) not null
+);
+
+create table scores
+(
+    username varchar(20) not null primary key,
+    score int not null,
+    foreign key (username) references users(username)
 );
