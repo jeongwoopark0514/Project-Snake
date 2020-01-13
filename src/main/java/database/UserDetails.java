4e1bf5d4db572f79ef70f64165282f2063a7769b
Rohan Deshamudre <deshamudreshivadarshanrohan@student.tudelft.nl>
2020-01-13 13:30:03 +0100

    add POJO class for fxml table view

---
+++ src/main/java/database/UserDetails.java
@@ -0,0 +1,53 @@
+package database;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class UserDetails {
+
+    private SimpleIntegerProperty rank;
+    private SimpleStringProperty username;
+    private SimpleIntegerProperty score;
+
+    public UserDetails(Integer rank, String username, Integer score){
+        this.rank = new SimpleIntegerProperty(rank);
+        this.username = new SimpleStringProperty(username);
+        this.score = new SimpleIntegerProperty(score);
+    }
+
+    public int getRank() {
+        return rank.get();
+    }
+
+    public SimpleIntegerProperty rankProperty() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = new SimpleIntegerProperty(rank);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = new SimpleStringProperty(username);
+    }
+
+    public int getScore() {
+        return score.get();
+    }
+
+    public SimpleIntegerProperty scoreProperty() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = new SimpleIntegerProperty(score);
+    }
+}
