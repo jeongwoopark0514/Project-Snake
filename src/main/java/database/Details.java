741a46e1f365bbd97290d9f6304645dc374f0144
Leon de Klerk <l.p.j.deklerk@student.tudelft.nl>
2020-01-23 19:28:34 +0100

    add JavaDoc remove excess methods

--- src/main/java/database/Details.java
+++ src/main/java/database/Details.java
@@ -3,8 +3,11 @@ package database;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * POJO class that contains all the data that is retrieved from the database,
+ * which needs to be put into the lists of the leaderboard.
+ */
 public class Details {
-
     private SimpleIntegerProperty rank;
     private SimpleStringProperty name;
     private SimpleIntegerProperty score;
@@ -12,9 +15,9 @@ public class Details {
     /**
      * Constructor for POJO class.
      *
-     * @param rank  - the rank of players highscore
-     * @param name  - name of player
-     * @param score - score of the player
+     * @param rank  the rank of players highscore
+     * @param name  name of player
+     * @param score score of the player
      */
     public Details(Integer rank, String name, Integer score) {
         this.rank = new SimpleIntegerProperty(rank);
@@ -22,39 +25,30 @@ public class Details {
         this.score = new SimpleIntegerProperty(score);
     }
 
+    /**
+     * Get the rank of the player.
+     *
+     * @return the rank
+     */
     public int getRank() {
         return rank.get();
     }
 
-    public void setRank(int rank) {
-        this.rank = new SimpleIntegerProperty(rank);
-    }
-
-    public SimpleIntegerProperty rankProperty() {
-        return rank;
-    }
-
+    /**
+     * Get the name of the user.
+     *
+     * @return the name
+     */
     public String getName() {
         return name.get();
     }
 
-    public void setName(String name) {
-        this.name = new SimpleStringProperty(name);
-    }
-
-    public SimpleStringProperty nameProperty() {
-        return name;
-    }
-
+    /**
+     * Get the score of the user.
+     *
+     * @return the score
+     */
     public int getScore() {
         return score.get();
-    }
-
-    public void setScore(int score) {
-        this.score = new SimpleIntegerProperty(score);
-    }
-
-    public SimpleIntegerProperty scoreProperty() {
-        return score;
     }
 }
--- src/test/java/database/DetailsTest.java
+++ src/test/java/database/DetailsTest.java
@@ -1,7 +1,6 @@
 package database;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,53 +13,14 @@ class DetailsTest {
     }
 
     @Test
-    void rankPropertyTest() {
-        Details details = new Details(1, "name", 30);
-        assertNotNull(details.rankProperty());
-    }
-
-    @Test
-    void setRankTest() {
-        Details details = new Details(1, "name2", 20);
-        details.setRank(12);
-        assertEquals(12, details.getRank());
-    }
-
-    @Test
     void getNameTest() {
         Details details = new Details(1, "name3", 30);
         assertEquals("name3", details.getName());
     }
 
     @Test
-    void namePropertyTest() {
-        Details details = new Details(4, "name31", 33);
-        assertNotNull(details.nameProperty());
-    }
-
-    @Test
-    void setNameTest() {
-        Details details = new Details(1, "name", 30);
-        details.setName("namename");
-        assertEquals("namename", details.getName());
-    }
-
-    @Test
     void getScoreTest() {
         Details details = new Details(1, "nameqw", 30);
         assertEquals(30, details.getScore());
-    }
-
-    @Test
-    void scorePropertyTest() {
-        Details details = new Details(1, "nameqw", 30);
-        assertNotNull(details.scoreProperty());
-    }
-
-    @Test
-    void setScoreTest() {
-        Details details = new Details(1, "sjkf", 234);
-        details.setScore(23442);
-        assertEquals(23442, details.getScore());
     }
 }
